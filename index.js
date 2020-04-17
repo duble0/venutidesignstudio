a905b47eaded92246f1b03270517f6deb586eb42
duble0 <duble0venutifrancesco@gmail.com>
2020-04-17 23:43:18 +0200

    rev27

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //index.js  
 function sendEmail() {
 	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	Password : "<email password>",
-	To : '<recipient’s email address>',
-	From : "<sender’s email address>",
-	Subject : "<email subject>",
-	Body : "<email body>",
+				Token : "2857c539-a94d-4b30-a15f-18fd410a3dcf"
+				To : '<v.f.d@outlook.it>',
+				Name : "<sender’s name>",
+				From : "<sender’s email address>",
+				Subject : "<email subject>",
+				Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
 	);
